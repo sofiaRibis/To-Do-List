a44dcf266b4a865618fed350f5c1abaf712d31b3
sofiaRibis <sofia.ribis@gmail.com>
2022-02-09 17:32:44 +0100

    non so

---
+++ script.js
@@ -0,0 +1,13 @@
+//se volgio il nome del giorno
+const week = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById('week-name').innerHTML = days[week.getDay()]
+
+//se volgio il numero del giorno
+const d = new Date();
+document.getElementById("day-number").innerHTML = d.getDate();
+
+//se voglio il nome del mese
+const month = new Date();
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById('month').innerHTML = months[month.getMonth()]
