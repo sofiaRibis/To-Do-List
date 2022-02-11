5b126fc549aede6a49b5cb62aa92e9bafbe2ef59
sofiaRibis <sofia.ribis@gmail.com>
2022-02-11 16:44:51 +0100

    style-js: checked items

--- script.js
+++ script.js
@@ -11,3 +11,35 @@ document.getElementById("day-number").innerHTML = d.getDate();
 const month = new Date();
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 document.getElementById('month').innerHTML = months[month.getMonth()]
+
+
+//per quando gli li vengono flaggati
+let checkedItems = document.querySelectorAll('label input');
+let listItems = document.querySelectorAll('label');
+let text = document.querySelectorAll('label p');
+
+
+checkedItems.forEach(checkedItem => {
+    checkedItem.addEventListener('click', function () {
+
+        console.log(checkedItem.checked);
+
+        if (checkedItem.checked === true) {
+            listItems.forEach(item => {
+                item.classList.add('checked-item');
+            });
+
+            text.forEach(item => {
+                item.classList.add('checked-task');
+            });
+        } else {
+            listItems.forEach(item => {
+                item.classList.remove('checked-item');
+            });
+
+            text.forEach(item => {
+                item.classList.remove('checked-task');
+            });
+        }
+    });
+});
