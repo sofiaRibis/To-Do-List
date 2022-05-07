7009d41e4d642048bed88a5b7f5a36fe5459ca03
sofiaRibis <sofia.ribis@gmail.com>
2022-05-07 22:09:57 +0200

    list style

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 //se volgio il nome del giorno
 const week = new Date();
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-document.getElementById('week-name').innerHTML = days[week.getDay()] + ',';
+let days = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
+document.getElementById("week-name").innerHTML = days[week.getDay()] + ",";
 
 //se volgio il numero del giorno
 const d = new Date();
@@ -9,82 +17,85 @@ document.getElementById("day-number").innerHTML = d.getDate();
 
 //se voglio il nome del mese
 const month = new Date();
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-document.getElementById('month').innerHTML = months[month.getMonth()];
+let months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
+document.getElementById("month").innerHTML = months[month.getMonth()];
 
 //* VARIABLES
 //launch popup
-let LaunchPopupDiv = document.querySelector('.Launch-popup');
-let launchBtn = document.querySelector('.Launch-popup button');
-let main = document.querySelector('main');
+let LaunchPopupDiv = document.querySelector(".Launch-popup");
+let launchBtn = document.querySelector(".Launch-popup button");
+let main = document.querySelector("main");
 
 //per quando gli li vengono flaggati
-let checkedItems = document.querySelectorAll('label input');
-let listItems = document.querySelectorAll('label');
-let text = document.querySelectorAll('p .list-item');
-let li = document.querySelectorAll('li');
+let checkedItems = document.querySelectorAll("label input");
+let listItems = document.querySelectorAll("label");
+let text = document.querySelectorAll("p .list-item");
+let li = document.querySelectorAll("li");
 
 //modal pop up
-let addTask = document.querySelector('.addTask');
-let modalPopupTask = document.querySelector('.modal-add-task');
-let saved = document.querySelector('input[type=submit]');
+let addTask = document.querySelector(".addTask");
+let modalPopupTask = document.querySelector(".modal-add-task");
+let saved = document.querySelector("input[type=submit]");
 
-//create new element 
-let inputValue = document.querySelector('.popup input');
-let listWrapper = document.querySelector('ul');
+//create new element
+let inputValue = document.querySelector(".popup input");
+let listWrapper = document.querySelector("ul");
 
-//delete item 
-let trash = document.querySelectorAll('.delete');
+//delete item
+let trash = document.querySelectorAll(".delete");
 
 //======================================================
 
 //launch popup
-launchBtn.addEventListener('click', function () {
-    LaunchPopupDiv.style.display = 'none';
-    main.style.display = 'flex';
+launchBtn.addEventListener("click", function () {
+	LaunchPopupDiv.style.display = "none";
+	main.style.display = "flex";
 });
 
-//create new element 
-//todo: non cambino style se le clicco 
-saved.addEventListener('click', function () {
-    if (inputValue != '') {
-        let newToDo = document.createElement('li');
-        newToDo.innerHTML = `
-                <label>
-                    <input type="checkbox">
-                    <p class="list-item">${inputValue.value}</p>
-                    <div class="delete">
-                        <img src="images/delete.svg" alt="delete icon">
-                    </div>
-                </label>
-            
-        `;
-        inputValue.value = '';
-        listWrapper.appendChild(newToDo);
-    }
+//create new element
+//todo: non cambino style se le clicco
+saved.addEventListener("click", function () {
+	if (inputValue != "") {
+		let newToDo = document.createElement("li");
+		newToDo.innerHTML = `
+                    <label>
+                        <input type="checkbox">
+                        <p class="list-item">${inputValue.value}</p>
+                        <div class="delete">
+                        </div>
+                    </label>
+                    
+                    `;
+		// <img src="images/delete.svg" alt="delete icon">
+		inputValue.value = "";
+		listWrapper.appendChild(newToDo);
+	}
 
-    modalPopupTask.classList.remove('flex-popup');
-
+	modalPopupTask.classList.remove("flex-popup");
 });
 
 //modal pop up
-addTask.addEventListener('click', function () {
-    modalPopupTask.classList.add('flex-popup');
+addTask.addEventListener("click", function () {
+	modalPopupTask.classList.add("flex-popup");
 });
 
-
-
-
-
-//delete item 
-trash.forEach(el => {
-    el.addEventListener('click', function () {
-
-        li.forEach(item => {
-            item.style.display = 'none';
-        });
-
-    });
+//see if item is checked or not
+checkedItems.forEach((element) => {
+	element.addEventListener("click", function (e) {
+		const ciao = e.target;
+		console.log(ciao);
+	});
 });
-
-
